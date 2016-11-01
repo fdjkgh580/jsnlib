e3661cca1a6e6664af1bcd64ec5f23cd69fccd49
jsn 筆電 <fdjkgh580@yahoo.com.tw>
2016-11-01 17:28:33 +0800

    update Jsnupload

--- jsnupload/jsnupload.php
+++ jsnupload/jsnupload.php
@@ -5,8 +5,6 @@ class Jsnupload {
 	public	$arraykey;					//input的name屬性鍵值				ex.	input="upl[0]"	的 0
 	public	$pathaccess;				//路徑中或資料夾的權限				ex.0755 或最高0777
 
-
-
 	//黑白名單擇一使用, 通常使用白名單自訂允許的檔案會比較安全
 	public	$blacklist;					//黑名單的副檔明,	用逗號分開
 	public 	$could_secondname; 			//允許的副檔名,		用逗號分開
@@ -16,8 +14,6 @@ class Jsnupload {
 	public	$newname;					//新檔名
 	
 	
-	
-	
 	//套件ImageResize 調整圖片大小相關設定
 	public	$resizeImageScriptPath;		//套件ImageResize 路徑
 	public	$resize_width;				//圖片重新調整寬
@@ -25,24 +21,27 @@ class Jsnupload {
 	public	$resize_type;				//套件ImageResize 縮放的類型(預設1) 不然有一邊會超過大小, 特殊使用 or 根據最大長度來判斷(可保證在指定大小內))
 	public	$resize_quality;			//壓縮品質 (預設100)
 	
-	function __construct(){
+	function __construct()
+	{
 		$this->arraykey			=	0;
 		$this->pathaccess		=	"0777";
 		$this->resize_width		=	"1000";
 		$this->resize_height	=	"1000";
 		$this->resize_type		=	1;
 		$this->resize_quality	=	100;
-		}
+	}
 	
 	//錯誤代碼
-	function error_val(){
+	function error_val()
+	{
 		$filename	=	$this->filename;
 		$arykey		=	$this->arraykey;
 		return $_FILES[$filename]['error'][$arykey];
-		}
+	}
 		
 	//上傳檔案的副檔名	$Lower=1全部轉成小寫
-	function scandN($lower=0){
+	function scandN($lower=0)
+	{
 		$arykey		=	$this->arraykey;
 		$name		=	$this->filename;
 		$name		=	$_FILES[$name]['name'][$arykey];
@@ -50,10 +49,11 @@ class Jsnupload {
 		$scandN		=	ltrim($scandN,".");
 		if ($lower == "1") $scandN = strtolower($scandN);		//小寫
 		return $scandN;
-		}
+	}
 		
 	//檢查副檔名的黑名單	
-	function chk_blacklist(){
+	function chk_blacklist()
+	{
 
 		//分解字串放入陣列
 		$str		=	$this->blacklist;
@@ -71,11 +71,12 @@ class Jsnupload {
 				}			
 			}		
 		return 1;
-		}	
+	}	
 		
 		
 	//檢查允許的副檔名
-	function chk_whitelist(){
+	function chk_whitelist()
+	{
 		
 		//分解字串放入陣列
 		$str		=	$this->could_secondname;
@@ -90,20 +91,22 @@ class Jsnupload {
 				}
 			}			
 		return 0;
-		}	
+	}	
 	
 	//檢查大小
-	function chk_size(){
+	function chk_size()
+	{
 		$name		=	$this->filename;
 		$arykey		=	$this->arraykey;
 		$setsize	=	$this->size * 1000 * 1000; //MB
 		$filesize	=	$_FILES[$name]['size'][$arykey];
 		if ($filesize > $setsize) return 0;
 		return 1;
-		}
+	}
 	
 	//驗證檔案的指定型態
-	function chk_type($needtype){
+	function chk_type($needtype)
+	{
 		$name	=	$this->filename;
 		$arykey	=	$this->arraykey;
 		$type	=	$_FILES[$name]['type'][$arykey];
@@ -111,10 +114,11 @@ class Jsnupload {
 		
 		if ($type != $needtype) return 0;
 		return 1;
-		}	
+	}	
 
 	//準備好路徑
-	function setuplpath(){
+	function setuplpath()
+	{
 		$site	=	$this->site;
 		//分解並依序往下檢查、建立路徑(資料夾)
 		$ary	=	explode("/",$site);
@@ -132,51 +136,34 @@ class Jsnupload {
 			
 			die("不可寫入：{$realpath}，權值是：{$perms}");		
 		}
-		//chmod($realpath, $access) or die("路徑權限修改失敗，請手動修改權限：" . $realpath);
 		
-		
-		
-		/*
-		foreach($ary as $key => $createsite) {
-			
-			if (empty($ary[$key])) continue;
-			
-			//若使用路徑 ../jsnupload/album ，在自動建立相對路徑時，會因為建立「..」而產生錯誤，待修正
-			//windows平台會有$_SERVER['WINDIR']參數
-			if (!empty($_SERVER['WINDIR'])) 	unset($SysBef);
-			else 								$SysBef = "/";
-			
-			// 取消自動建立目錄功能
-			//if (!file_exists($SysBef.$mix.$createsite))  mkdir($SysBef.$mix.$createsite, $access) or die("路徑建置錯誤:" .$mix.$createsite);
-			//chmod($mix.$createsite, $access); //變更檔案存取權限, 避免預設伺服器建立時是755
-			
-			$mix		=	$mix.$createsite."/"; 
-			}
-		*/
 		return 1;
-		}
+	}
 	
 	//讓字串結尾保持 「 / 」 
-	function auto_endslash($string){
+	function auto_endslash($string)
+	{
 		$endstr		=	substr($string, -1);
 		$token		=	"/";
 		if ($endstr == $token) {
 			unset($token);
 			}
 		return $string.$token;
-		}
+	}
 		
 	
 	//遇到未指定上傳檔案的就換下一個<input> 
-	function isnextkey($key){
+	function isnextkey($key)
+	{
 		if (!empty($key)) return "0";
 		$this->arraykey += 1; //
 		return "1";
-		}
+	}
 		
 	
 	//驗證所有問題
-	function chk_all(){
+	function chk_all()
+	{
 		$filename			=	$this->filename;
 		$arykey				=	$this->arraykey;
 		$original_file		=	$_FILES[$filename]['name'][$arykey]; //原始檔名+附檔名
@@ -205,13 +192,15 @@ class Jsnupload {
 		
 		if (!empty($bla) and !empty($whi)) 	die("黑、白名單請擇一設置。");
 
-		elseif (!empty($bla)) {
+		elseif (!empty($bla)) 
+		{
 			if ($blacklist	==	0) die("不允許的檔案型態 : {$original_file}");
-			}
+		}
 			
-		elseif (!empty($whi)) { 
+		elseif (!empty($whi)) 
+		{ 
 			if ($whitelist	==	0) die("不允許的檔案型態 : {$original_file}");
-			}
+		}
 		
 		//3.檔案大小
 		$filesize	=	$_FILES[$filename]['size'][$arykey] / 1000 / 1000;	//上傳大小
@@ -224,10 +213,11 @@ class Jsnupload {
 		
 		//100.檢驗完成
 		return 1;
-		}
+	}
 	
 	//組合路徑與檔案完整名稱的字串
-	function mix_path_file(){
+	function mix_path_file()
+	{
 		//指定位置
 		$newname	=	$this->newname;
 		$site		=	$this->site;
@@ -235,10 +225,11 @@ class Jsnupload {
 		$site		=	$this->auto_endslash($site);
 		$site		=	$site.$newname;
 		return $site;
-		}
+	}
 	
 	//開始上傳 
-	function start_upload(){
+	function start_upload()
+	{
 		//來源
 		$name		=	$this->filename;
 		$arykey		=	$this->arraykey;		
@@ -248,20 +239,22 @@ class Jsnupload {
 		$site		=	$this->mix_path_file();
 		copy($filetemp,$site) or die("上傳錯誤: copy({$filetemp}, {$site})也有可能是上傳的路徑沒有權限寫入。");		//使用copy事後須刪除
 		return 1;
-		}
+	}
 		
 	//結束上傳	
-	function end_upload(){
+	function end_upload()
+	{
 		//清空暫存檔
 		$name		=	$this->filename;
 		$arykey		=	$this->arraykey;
 		$filetemp	=	$_FILES[$name]['tmp_name'][$arykey];
 		unlink($filetemp);
 		return 1;
-		}	
+	}	
 		
 	//重新變更圖片大小
-	function resizeImage($ImageResizeScriptPath){
+	function resizeImage($ImageResizeScriptPath)
+	{
 		include_once($ImageResizeScriptPath);
 		
 		$chktype	=	$this->chk_type("image");
@@ -276,58 +269,7 @@ class Jsnupload {
 		$result	=	ImageResize($site,$site,$neww,$newh,$retype,$req);	//成功返回存放路徑 失敗返回false
 		if (!$result)	return 0;
 		return 1;
-		}
-	
-	
-	/*
-	//將以fileupload_multi()取代
-	
-	//以陣列方式 執行function
-	function fileupload($resizeImg=0){
-		
-		//驗證無誤?
-		$prepare	=	$this->chk_all();
-		if ($prepare != 1) return 0;
-		
-		//開始上傳
-		$this->start_upload();
-		
-		//調整圖片大小(已寫自動判定格式)
-		if ($resizeImg == 1) {
-			$Scpath	=	$this->resizeImageScriptPath;				//套件路徑
-			$resultRE	=	$this->resizeImage($Scpath);
-			if ($resultRE != 1) echo ("調整錯誤，圖片仍保持原始大小"); 
-			}
-		
-		$success	=	$this->end_upload();						//清空暫存
-		return $success;
-		}	
-	
-	function fileupload2($newname, $add_arraykey, $resizeImg=0){
-		
-		$this->newname 				= 	$newname;					//建議：新檔名(時間+鍵值+副檔名)
-
-		//驗證無誤?
-		$prepare	=	$this->chk_all();
-		if ($prepare != 1) return 0;
-		
-		$this->start_upload();										//開始上傳
-		
-		//調整圖片大小(已寫自動判定格式)
-		if ($resizeImg == 1) {
-			$Scpath		=	$this->resizeImageScriptPath;				//套件路徑
-			$resultRE	=	$this->resizeImage($Scpath);
-			if ($resultRE != 1) echo ("調整錯誤，圖片仍保持原始大小"); 
-			}
-		
-		$success	=	$this->end_upload();						//清空暫存
-		
-		$this->arraykey = $add_arraykey +1;
-		
-		return $success;
-		}		
-	*/
-	
+	}
 	
 		
 	//傳遞參數陣列
@@ -335,7 +277,8 @@ class Jsnupload {
 	// $add_arraykey : input name 陣列起鍵值, 通常是從頭開始所以填0
 	// $resizeImg : 是否啟用縮圖
 	// $endupload : 對於暫存檔的使用。clean為清空、retain為保留
-	function fileupload_multi($newname, $add_arraykey, $resizeImg = 0, $endupload){
+	function fileupload_multi($newname, $add_arraykey, $resizeImg = 0, $endupload)
+	{
 		
 		$this->newname 				= 	$newname;					//建議：新檔名(時間+鍵值+副檔名)
 
@@ -346,11 +289,12 @@ class Jsnupload {
 		$this->start_upload();										//開始上傳
 		
 		//調整圖片大小(已寫自動判定格式)
-		if ($resizeImg == 1) {
+		if ($resizeImg == 1) 
+		{
 			$Scpath		=	$this->resizeImageScriptPath;				//套件路徑
 			$resultRE	=	$this->resizeImage($Scpath);
 			if ($resultRE != 1) echo ("調整錯誤，圖片仍保持原始大小"); 
-			}
+		}
 		
 		/*
 		 * [關於參數endupload]
@@ -363,14 +307,14 @@ class Jsnupload {
 		 *   直到最後一個比例時才使用 ($endupload = 1)
 		*/
 		
-		if ($endupload == "clean") {
+		if ($endupload == "clean") 
+		{
 			$success	=	$this->end_upload();						//清空暫存
 			$this->arraykey = $add_arraykey +1;							//接著準備上傳下一個<input>吧
-			}
+		}
 		
 			
 		return $success;
-		}		
 	}		
+}		
 
-?>
