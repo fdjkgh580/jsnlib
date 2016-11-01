254159ce903854d70e5aa589c3085e7263e84513
jsn 筆電 <fdjkgh580@yahoo.com.tw>
2016-11-01 16:12:06 +0800

    Cart update

--- Cart/Cart.php
+++ Cart/Cart.php
@@ -7,22 +7,25 @@ class Cart {
 	public $sess = 'session_jsncar';
 
 	//參數是否為陣列
-	private function param_isarray($param) { 
+	private function param_isarray($param) 
+	{ 
 		if (!is_array($param)) die('參數須要是陣列型態');
 		if (!empty($param['option']) and !is_array($param['option'])) die("參數option須要是陣列型態");
 		return "1";
-		}
+	}
 	
 	//檢查必要參數
-	private function required($param) {
+	private function required($param) 
+	{
 		$this->param_isarray($param);
-		$required = array(
+		$required = array
+		(
 			'primaryid'	=>	0,
 			'name'	=>	0,
 			'quantity'	=>	0,
 			'price'		=>	0,
 			'option'	=>	0
-			);
+		);
 		
 		//有出現的鍵	
 		foreach ($param as $key => $val) $required[$key] = 1;
@@ -32,25 +35,28 @@ class Cart {
 		if (!empty($res_key)) die("請指定參數：{$res_key}");
 		
 		return $this;
-		}
+	}
 	
 	//加入購物車的是新商品？
-	public function isnew($primaryid) {
+	public function isnew($primaryid) 
+	{
 		$key = $primaryid;
 		return empty($_SESSION[$this->sess][$key]) ? "1" : "0";
-		}	
+	}	
 
 	//單項產品加總
-	private function single_count($param) {
+	private function single_count($param) 
+	{
 		$key		= $param['primaryid'];
 		$price		= $_SESSION[$this->sess][$key]['price'];
 		$quantity	= $_SESSION[$this->sess][$key]['quantity'];
 		$_SESSION[$this->sess][$key]['count'] = $price * $quantity;
 		return "1";
-		}
+	}
 		
 	//新增 (回傳1新增成功; 回傳0代表商品已存在)
-	public function insert($param){
+	public function insert($param)
+	{
 		$this->required($param);
 		$isnew = $this->isnew($param['primaryid']);
 		if ($isnew == "0") return "0";
@@ -63,11 +69,11 @@ class Cart {
 		$_SESSION[$this->sess][$key]['option']		= $param['option'];
 		$this->single_count($param);
 		return "1";
-		}
+	}
 	
 	//修改
-	public function update($param){
-		
+	public function update($param)
+	{
 		$isnew = $this->isnew($param['primaryid']);
 		if (empty($param['primaryid'])) die('請指定修改的商品primaryid');
 		
@@ -77,45 +83,51 @@ class Cart {
 		$this->param_isarray($param);
 		
 		//當修改quantity為0時將視同刪除
-		if (isset($param['quantity']) and $param['quantity'] == "0"){
+		if (isset($param['quantity']) and $param['quantity'] == "0")
+		{
 			$this->delete($param['primaryid']);
 			return 1;
-			}
+		}
 		
 		$item = $param['primaryid'];
-		foreach ($param as $key => $val) {
+		foreach ($param as $key => $val) 
+		{
 			if ($key == 'primaryid') continue;
 			$_SESSION[$this->sess][$item][$key] = $val;
 			$this->single_count($param);
-			}
-		return "1";
 		}
+		return "1";
+	}
 	
 	
 	//刪除
-	public function delete($primaryid){
+	public function delete($primaryid)
+	{
 		//若商品本身不存在
 		if (empty($_SESSION[$this->sess][$primaryid])) return "0";
 		
 		unset($_SESSION[$this->sess][$primaryid]);
 		return "1";		
-		}
+	}
 	
 	// 取得已在購物車的商品資訊
-	public function get($primaryid) {
+	public function get($primaryid) 
+	{
 		$res = $_SESSION[$this->sess][$primaryid];
 		return empty($res) ? "0" : $res;
-		}
+	}
 
 
 	//清空購物車
-	public function truncate() {
+	public function truncate() 
+	{
 		unset($_SESSION[$this->sess]);
 		return empty($_SESSION[$this->sess]) ? "1" : "0";
-		}
+	}
 
 	// 排除的項目
-	protected function exclude($order, array $exclude = NULL) {
+	protected function exclude($order, array $exclude = NULL) 
+	{
 		$newary = array();
 		if (!is_array($exclude)) return $order;
 		foreach ($order as $key => $val)
@@ -132,7 +144,8 @@ class Cart {
 	 * 取得帳單
 	 * @param   $exclude 排除的項目
 	 */
-	public function order(array $exclude = NULL){
+	public function order(array $exclude = NULL)
+	{
 		$order = is_array($_SESSION[$this->sess]) ?  $_SESSION[$this->sess] : array();
 		$result = $this->exclude($order, $exclude);
 		return $result;
@@ -153,9 +166,5 @@ class Cart {
 		}
 		return $total;
 	}
-	
+}
 
-
-	}
-
-?>
