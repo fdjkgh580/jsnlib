8091c72e72a75aa241c88fb9b6f38ab0fb7a0df3
jsn 筆電 <fdjkgh580@yahoo.com.tw>
2016-11-01 16:57:48 +0800

    delete jsnbgpopen.php

--- jsnbgpopen/script_1.php
+++
@@ -1,8 +0,0 @@
-<?
-while($a < 3)
-{
-	mkdir("dir_from_script_1_" . uniqid());
-	$a++;
-	sleep(3);
-}
-?>
--- jsnbgpopen/script_2.php
+++
@@ -1,8 +0,0 @@
-<?
-while($a < 3)
-{
-	mkdir("dir_from_script_2_" . uniqid());
-	$a++;
-	sleep(5);
-}
-?>
